32a069164457e62bb915c35e2732d4eba809a40c
Raul Condolo <kevincondolo@gmail.com>
2023-05-24 11:26:33 -0500

    Carga de backend inicial

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+//express server
+const app = express();
+
+//requests
+app.listen(4000, ()=>{
+    console.log(`Group MIND server is running on port ${4000}`)
+
+})
